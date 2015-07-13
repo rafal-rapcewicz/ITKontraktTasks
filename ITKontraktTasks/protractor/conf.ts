99bda6195a0da1bb48dac8f18f43900f1fb5b51d
rafal-rapcewicz <rafal.rapcewicz@gmail.com>
2015-07-13 20:02:10 +0200

    not working tests added

---
+++ ITKontraktTasks/protractor/conf.ts
@@ -0,0 +1,6 @@
+﻿declare var exports: any;
+
+exports.config = {
+    seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+    specs: ['./hundretElements/test.spec.js']
+}; 
---
+++ ITKontraktTasks/protractor/hundretElements/test.spec.ts
@@ -0,0 +1,24 @@
+﻿/// <reference path="../../scripts/typings/jasmine/jasmine.d.ts" />
+
+declare var browser, element, by: any;
+
+describe("Hundred elements:", function () {
+    describe("on loaded:", function () {
+        it("should be 100 of them", function () {
+            //browser.get("http://localhost:3000/");
+            browser.get("../../index.html");
+
+            var el = element(by.css('[ng-change="vm.swap(1)"]'));
+
+            var el2 = el.findElement(by.xpath('..'));
+
+            var checkbox1Text;
+            el2.getText().then(function (text) {
+                checkbox1Text = text;
+            });
+
+            expect(el2.getText()).toEqual('Show left sidebar');
+  
+        });
+    });
+}); 
